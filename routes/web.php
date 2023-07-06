4a38ce3212c95b4a9e922ebafb13fab66024dc99
behin-pixel <behin.pixel@gmail.com>
2023-07-06 20:39:22 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['auth_users']], function () {
     \UniSharp\LaravelFilemanager\Lfm::routes();
 });
- Route::get('/', function(){
-      return view('website.index');
-  });
-
-//  Route::get('/',[ConsultantFormController::class,'index'])->name('home');
+//  Route::get('/', function(){
+//       return view('website.index');
+//   });
+  Route::get('/',[ConsultantFormController::class,'index'])->name('home');
 
 // Route::get('/hospitals/gericare-hospitals',function(){
 //     return view('website.hospitals.gericare-hospitals');
