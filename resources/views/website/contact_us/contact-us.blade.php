3325a1952f25ede05368922ff8e8a47e709c7057
behin-pixel <behin.pixel@gmail.com>
2023-07-11 18:53:10 +0530

    terms

--- resources/views/website/contact_us/contact-us.blade.php
+++ resources/views/website/contact_us/contact-us.blade.php
@@ -109,7 +109,7 @@ to improve the lives of the elderly.</span>
                 <div class="col-lg-12 margin-top-50">
 
 				<div class="dark-gray-bg text-center">
-				<h2 class="h3 mar-bot-20">Our locations</h2>
+				<h2 class="h3 mar-bot-20">Our Locations</h2>
 				</div>
 <div class="serveces-doctrs dark-gray-bg text-center">
 
