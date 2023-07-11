2de5ca77073e25e83781108d8ac194ad90637181
behin-pixel <behin.pixel@gmail.com>
2023-07-11 12:23:21 +0530

    Pull

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,6 @@ Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['auth_users']]
 //  Route::get('/', function(){
 //       return view('website.index');
 //   });
-
-  Route::get('/error', function(){
-       return view('errors.404');
-   });
   Route::get('/',[ConsultantFormController::class,'index'])->name('home');
 
 // Route::get('/hospitals/gericare-hospitals',function(){
