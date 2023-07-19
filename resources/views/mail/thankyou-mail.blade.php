94b8535bef85dfa3025884b578dcc4071a964185
behin-pixel <behin.pixel@gmail.com>
2023-07-19 17:16:01 +0530

    ty

--- resources/views/mail/thankyou-mail.blade.php
+++ resources/views/mail/thankyou-mail.blade.php
@@ -1,43 +1,43 @@
 
-<table width="100%" border="0" cellspacing="0" cellpadding="0" style="background: #f5f8fa;">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="background: #f5f8fa; border:0px; ">
     <tbody>
       <tr>
-        <td style="padding: 30px;"><table width="500" border="0" align="center" cellpadding="0" cellspacing="0" style="background: #fff;">
+        <td style="padding: 30px; border:0px;"><table width="500" border="0" align="center" cellpadding="0" cellspacing="0" style="background: #fff; border:0px;">
           <tbody>
             <tr>
-              <td align="center" style="background: #7C164B; padding: 15px; " ><img src="{{ asset('/public/website/assets/images/logo-w.png') }}" width="374" height="75" alt="Geri Care Hospital" /></td>
+              <td align="center" style="background: #7C164B; padding: 15px; border:0px; " ><img src="{{ asset('/public/website/assets/images/logo-w.png') }}" width="374" height="75" alt="Geri Care Hospital" /></td>
             </tr>
             <tr>
-              <td align="center" style="padding:20px; color: #8e8e8e; font-family:Helvetica, Arial; font-size:22px;">Thank You!!!  </b> </td>
+              <td align="center" style="padding:20px; color: #8e8e8e; border:0px; font-family:Helvetica, Arial; font-size:22px;">Thank You!!!  </b> </td>
             </tr>
             <tr>
-              <td style="padding: 20px; "><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <td style="padding: 20px; border:0px; "><table width="100%" border="0" cellspacing="0" cellpadding="0" style="border:0px;">
                 <tbody>
                  <tr>
-                        <td height="30" style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; " >Dear {{ $data['name'] }}, </td>
+                        <td height="30" style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; border:0px; " >Dear {{ $data['name'] }}, </td>
                     </tr>
                     <tr>
-                        <td height="20" style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; " > </td>
+                        <td height="20" style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; border:0px; " > </td>
                     </tr>
                     <tr>
-                        <td style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; " >  
+                        <td style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; border:0px; " >  
                          <p>We are pleased to inform you that your appointment has been successfully scheduled.</p>
                          </td>
                     </tr>
                     <tr>
-                        <td height="20" style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; " > </td>
+                        <td height="20" style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; border:0px; " > </td>
                     </tr>                     
                     <tr>
-                        <td style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; " >
+                        <td style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; border:0px; " >
                            <p> In case you have any questions or need further information before the scheduled date, please feel free to reach out to us. We are more than happy to assist you and provide any details you may require.</p>
                         <p> Once again, thank you for choosing Geri Care. We value your time and trust in us, and we are committed to delivering a positive and rewarding experience during our upcoming appointment.</p>                           
                         </td>
                     </tr>
                     <tr>
-                        <td height="20" style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; " > </td>
+                        <td height="20" style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; border:0px; " > </td>
                     </tr>
                     <tr>
-                        <td style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; " >
+                        <td style="font-size: 16px; font-family:Helvetica, Arial; color: #454545; border:0px; " >
                           <p>Warm Regards</p>
                         </td>
                     </tr>
